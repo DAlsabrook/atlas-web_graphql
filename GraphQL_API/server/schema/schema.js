5398138be13d40dccc865f5a9e461649ef2d9388
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-11-01 11:47:29 -0500

    I think i resolved the export error

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -8,4 +8,24 @@ const TaskType = new GraphQLObjectType({
     weight: { type: GraphQLInt },
     description: { type: GraphQLString }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: (parent, args) => {
+        //logic here
+      }
+    }
+  },
+
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
