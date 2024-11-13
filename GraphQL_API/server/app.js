90cd5b8dd0011c82aa9be9ed9434aaf6e19bd71f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-11-13 12:21:43 -0600

    Connected to mongo and all is working. Took a while to get mongoose shema and the queries correct.

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -4,9 +4,11 @@ const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 
 const app = express();
+const connectionString = `mongodb+srv://dfalsabrook:FGKlcCtLJZl7X7yC@cluster0.4ys2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
-  console.log('connected to database');
+  console.log('Connected to MongoDB');
 });
 
 app.use('/graphql',graphqlHTTP({
